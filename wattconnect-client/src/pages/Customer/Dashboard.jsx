71279cf758b3e77c36661d2610f923ca88af5807
Diya Saha <168462932+Diya-Saha2005@users.noreply.github.com>
2025-06-25 22:02:54 +0530

    Corrections in the dashboard page

--- wattconnect-client/src/pages/Customer/Dashboard.jsx
+++ wattconnect-client/src/pages/Customer/Dashboard.jsx
@@ -1,7 +1,5 @@
 import Navbar from "../../components/Navbar";
 import { useNavigate } from "react-router-dom";
-import LineChart from "../../components/LineChart";
-import { interpolationChartData } from "../../assets/fakedata";
 import TariffChart from "../../components/TariffChart";
 
 const username = "Customer";
@@ -12,16 +10,6 @@ export default function CustomerDashboard() {
   const handleAddNew = () => {
     navigate("/customer/add");
   };
-
-  // Total Steps Calculation
-  const totalStepsByShaan = interpolationChartData.datasets[0].data.reduce(
-    (sum, val) => sum + val,
-    0
-  );
-  const totalStepsByShweta = interpolationChartData.datasets[1].data.reduce(
-    (sum, val) => sum + val,
-    0
-  );
 
   return (
     <div className="flex h-screen bg-[#f4f6fa]">
@@ -50,33 +38,6 @@ export default function CustomerDashboard() {
           <TariffChart />
         </div>
 
-        {/* Container: Summary + Chart */}
-        <div className="bg-white p-6 rounded-2xl shadow-md w-full flex flex-col md:flex-row gap-6 max-w-5xl">
-          {/* Left Column: Total Steps Cards */}
-          <div className="flex flex-col gap-6 md:w-1/4">
-            <div className="rounded-xl p-4 shadow-md">
-              <p className="text-sm text-gray-500">Total Steps by Shaan</p>
-              <h3 className="text-2xl font-semibold text-gray-700 mt-1">
-                {totalStepsByShaan.toLocaleString()} steps
-              </h3>
-            </div>
-
-            <div className="rounded-xl p-4 shadow-md">
-              <p className="text-sm text-gray-500">Total Steps by Shweta</p>
-              <h3 className="text-2xl font-semibold text-gray-700 mt-1">
-                {totalStepsByShweta.toLocaleString()} steps
-              </h3>
-            </div>
-          </div>
-
-          {/* Right Column: Line Chart */}
-          <div className="w-full md:w-2/3">
-            <h3 className="text-xl font-semibold text-gray-700 mb-4">
-              Monthly Steps Trend
-            </h3>
-            <LineChart />
-          </div>
-        </div>
       </main>
     </div>
   );
